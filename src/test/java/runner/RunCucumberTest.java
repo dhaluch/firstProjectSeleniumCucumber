8645ff0450dbfc806824fe3c09c01bc8ef66d1e3
Daniel <daniel.haluch@bluebear.app>
2022-12-30 18:35:19 -0300

    Adiconado os Packege pages, runner, steps and support. Adicionado a classe RunCucumberTest

---
+++ src/test/java/runner/RunCucumberTest.java
@@ -0,0 +1,32 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/reports/cucumberReport.Json", "html:target/reports/"},
+        features = "src/test/resources/features",
+        tags = {"~@ignore"},
+        glue = {"steps"}
+)
+public class RunCucumberTest {
+    public  static ChromeDriver driver;
+    public static ChromeOptions options;
+
+    @BeforeClass
+    public static void start(){
+        options = new ChromeOptions();
+        options.addArguments("star-maximized");
+        driver = new ChromeDriver(options);
+
+    }
+    @BeforeClass
+    public static void stop(){
+
+    }
+}
